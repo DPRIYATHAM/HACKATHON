49c61d36a0d7be2d40e5c6b708635e9a40bb9642
D PRIYATHAM <priyathamdarisi@gmail.com>
2022-01-14 21:33:26 +0530

    Create random_number_gen.py

---
+++ random_number_gen.py
@@ -0,0 +1,29 @@
+#This is a code to give random in the given range by user.
+
+#this code is made for MLH hackathon
+
+#local hack day.
+
+#by me Priyaham
+
+#coded on 14 January 2021
+
+import random 
+
+#take the input from user
+
+a = int(input("Enter the lower limit of range: "))
+b = int(input("Enter the upper limit of range: "))
+
+#define x
+
+x = random.randint(a,b)
+print("A random number between the given range"+a+"and"+b+"is:",x)
+
+#provide customer a vote of thanks.
+
+print("Thank you for using our MIXSER")
+print("Looking back for you to provide further services.")
+print("Team MIXSER")
+print("Managing Director")
+print("Priyatham Darisi")
