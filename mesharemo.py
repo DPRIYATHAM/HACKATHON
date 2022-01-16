99fd482115833159bbdb8b6b1f248ec37f865f32
D PRIYATHAM <priyathamdarisi@gmail.com>
2022-01-16 12:50:46 +0530

    Create mesharemo.py

---
+++ mesharemo.py
@@ -0,0 +1,46 @@
+#I am from India, here many of us give more value to emotions and relations 
+#nowadys I feel bad, as they are also getting decreased.
+#people are running behind money and living values and morals.
+#In this process they are loosing relations and not finding anyone who can support them are help in their sadness, share their happiness.
+# I want to do this project with some basic knowledge of python.
+"""In India the emotions are mainly devided into 9 types and they are
+Nine emotions are Shringara (love/beauty), Hasya (laughter), Karuna(sorrow), 
+Raudra (anger), Veera (heroism/courage), Bhayanaka (terror/fear),
+Bibhatsa (disgust), Adbutha (surprise/wonder), Shantha (peace or tranquility)"""
+# when we share one of the feelings with the computer it gives a output according to that.
+nam = str(input("Hi Dear user, What is your sweet name? : "))
+print("What is your current emotion?"+ nam)
+print("Select a emotion from the given list, please mind speeling an letters are case sensitive")
+print("Santha(peace/tranquility)")
+print("Adbutha (surprise/wonder)")
+print("Bibhatsa (disgust)")
+print("Bhayanaka (terror/fear)")
+print("Veera (heroism/courage)")
+print("Raudra (anger)")
+print("Karuna(sorrow)")
+print("Hasya (laughter)")
+print("Shringara (love/beauty)")
+print("Type the complete word here Example, if you want to input peace, please type: "Santha(peace/tranquility)" ") 
+feeling = str(input("Enter one of the specified emotion here : "))
+if feeling = Santha(peace/tranquility):
+    print("Thats really nice "+ nam + " keeping peace is a problem for many solutions. I am really happy as you are in Peace.")
+elif feeling = Adbutha (surprise/wonder):
+    print("These are common in the world "+ nam + " ,there are many wonders happening in universe at every movement, but we can't see all but if you just saw one, Congratulatins. U had a good day.")
+elif feeling = Bibhatsa (disgust):
+    print("I can understand the pain, but yeah still, it is life and aything can happen. We cannot change the moment " + nam + ". We are supposed to just move on. Forget the disgusting experience and move on.")
+elif feeling = Bhayanaka (terror/fear):
+    print("Ya I too get fear when ever I get a compilation error , though I am not stoped there itself "+ nam ".You just had one now, Feel free and go, just listen a song or do something you like most to do.")
+elif feeling = Veera (heroism/courage):
+    print("Wow, You are noted " + nam + ". The act which you made was so courageous, you will definitely get noticed. Check some posts on socila media, you may be taggd.😊")
+else:
+    print("I am sorry, " + nam + "I couldn't just get how are you feeling now, I will check my code once, meanwhile you also try to renter the feeling. Please enter the input in the format specified.")
+print("It was a good talk with you "+ nam + ". I hope my mesage got some interaction with you.")
+print("Looking forward to share some more feelings with you.")
+print("Have a good day "+ nam + ".")
+print("Thanks for using MESHAREMO")
+    
+    
+    
+    
+    
+    
